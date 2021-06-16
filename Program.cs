d7ed015e097b18b7b131433716f36334bb681f1b
AJeane <ajeane1924@gmail.com>
2021-06-15 22:25:06 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IterationStatements1
+{// insert line comment here
+    /*insert multi line comment here*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for (int i = 0; i < 16; i++)
+            {
+                Console.WriteLine($"{i}");
+
+            }
+
+
+
+    
+        }
+    }
+}
